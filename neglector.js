8b1fdd446d0530412eeab9d11131c7b71d7efdd1
SarCasm <sarcasm008@gmail.com>
2018-01-16 15:05:08 +0200

    Set up debbug loop

--- neglector.js
+++ neglector.js
@@ -1,10 +1,13 @@
 'use strict'
 
-var acorn = require("./node_modules/acorn/dist/acorn.js")
+var acorn = require("./node_modules/acorn/dist/acorn.js");
+const repl = require('repl');
 
 var neglect = function(sourceCode) {
-  debugger;
+  repl.start('> ');
   return acorn.parse(sourceCode);
 }
+
+debugger;
 
 module.exports = neglect
